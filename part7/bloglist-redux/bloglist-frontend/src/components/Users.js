10716c3b55dd529f57aa3aa86b99f5a8fb4be215
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-09-03 18:09:02 +0200

    completed 7.12 & 7.13

--- part7/bloglist-redux/bloglist-frontend/src/components/Users.js
+++ part7/bloglist-redux/bloglist-frontend/src/components/Users.js
@@ -1,18 +1,40 @@
-
+import { useSelector } from 'react-redux'
 
 const Users = (props) => {
+  const blogs = useSelector(state => state.blogs)
   const usersList = props.users
+  const tableStyle = {
+    minWidth: '400px',
+    border: '1px solid black',
+    textAlign: 'center'
+  }
+  const getBlogsQuanitity = (user) => {
+    let numberOfBlogs = 0
+    blogs.map(blog => {
+      if(blog.user.username === user.username){
+        numberOfBlogs+=1
+      }
+    })
+    return numberOfBlogs
+  } 
   if (usersList == undefined){
     return null
   }
   return(
     <div>
       <h3>Registered users:</h3>
-      <ul>
-        {usersList.map(user => 
-          <li key={user.id}>{user.name}</li>  
+      <table style={tableStyle}>      
+        <tr>
+          <th style={{border: '1px solid black'}}>User</th>
+          <th style={{border: '1px solid black'}}>Number of blogs</th>
+        </tr>
+        {usersList.map(user => (
+          <tr>
+            <td key={user.id}>{user.name}</td>  
+            <td>{getBlogsQuanitity(user)}</td>
+          </tr>)
         )}
-      </ul>
+      </table>
     </div>
   )
 }
