43863b273d1fddc82f6913b80aef4d923b5a8c97
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-09-13 20:47:17 +0200

    completed 8.10

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react'
-import { gql, useMutation } from '@apollo/client'
-
-const ADD_BOOK = gql`
-  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String]!) {
-    addBook(
-      title: $title,
-      author: $author,
-      published: $published,
-      genres: $genres
-    ) {
-      title
-      author
-      published
-      genres
-    }
-  }
-`
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK, ALL_BOOKS } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -23,7 +8,7 @@ const NewBook = (props) => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
-  const [ createBook ] = useMutation(ADD_BOOK)
+  const [ createBook ] = useMutation(ADD_BOOK, {refetchQueries: [{ query: ALL_BOOKS }]})
 
   if (!props.show) {
     return null
