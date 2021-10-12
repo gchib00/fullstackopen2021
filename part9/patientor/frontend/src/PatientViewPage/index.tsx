7543b22774dc67f6742fb9d7952f74e0a6007c38
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-10-12 22:48:33 +0200

    completed 9.21

--- part9/patientor/frontend/src/PatientViewPage/index.tsx
+++ part9/patientor/frontend/src/PatientViewPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Patient, BaseEntry } from "../types";
+import { Patient, BaseEntry, Diagnosis } from "../types";
 import { useParams } from "react-router";
 import { apiBaseUrl } from "../constants";
 import axios from "axios";
@@ -7,12 +7,18 @@ import { Icon } from 'semantic-ui-react';
 
 const PatientListPage = () => {
   const [patient, setPatient] = useState<Patient>();
+  const [diagnosesArr, setDiagnosesArr] = useState<Diagnosis[]>([]);
   const { id } = useParams<{id: string}>();
 
   const fetchFullPatient = async () => {
     const response = await axios.get<Patient>( `${apiBaseUrl}/api/patients/${id}`);
     const data: Patient = response.data;
     setPatient(data);
+    fetchDiagnoses();
+  }
+  const fetchDiagnoses = async () => {
+    const response = await axios.get<Diagnosis[]>(`${apiBaseUrl}/api/diagnoses`);
+    setDiagnosesArr(response.data);
   }
   useEffect(() => {
     fetchFullPatient();
@@ -29,8 +35,16 @@ const PatientListPage = () => {
       return 'mars';
     }
   }
+  const determineDiagnoses =  (code: string) => {
+    const response: Diagnosis | undefined = diagnosesArr.find(diagnosis => diagnosis.code == code);
+    if (!response){return null}
+    return response.name;
+  }
+
   const entriesArr: BaseEntry[] = patient.entries;
-  console.log(entriesArr)
+
+  console.log(diagnosesArr);
+
   return(
     <div>
       <h2>{patient.name} <Icon name={getGenderIcon()}/></h2>
@@ -38,26 +52,25 @@ const PatientListPage = () => {
       <p><strong>Occupation:</strong> {patient.occupation}</p>
       <br />
       <h3>Entries:</h3>
-        {entriesArr.map(entry => {
-          if(entry.diagnosisCodes === undefined){
+      {entriesArr.map(entry => {
+            if(!entry.diagnosisCodes){
+              return(<p>{entry.description}</p>)
+            }
             return(
-              <p>{entry.description}</p>
+              <div>
+                <p>{entry.description}</p>
+                <ul>
+                  {
+                    entry.diagnosisCodes.map(code => {
+                      const test = determineDiagnoses(code);
+                      if(typeof test === 'string'){return(<li key={code}>{code} - {test}</li>)}
+                      return(<li key={code}>{code}</li>)
+                    })
+                  }
+                </ul>
+              </div>
             )
-          }
-          return(
-            <div>
-              <p>{entry.description}</p>
-              <ul>
-                {entry.diagnosisCodes.map(code => {
-                  return(
-                    <li>{code}</li>
-                  )
-                })}
-              </ul>
-            </div>
-          )
         })}
-        {/* <p>{entriesArr.}</p> */}
     </div>
   )
 };
